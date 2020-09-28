dee52cd2be7f5162e9ef4043cdcfd4ba9c09a7e7
Abinas <youbeapro@gmail.com>
2020-09-28 00:11:33 -0400

    Fixed Email Address

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ module.exports = {
     photo: '/photo.jpg',
     bio: 'I am currently working in the United States as an international student.I will be sharing my step by step guide to apply student visa in the United States. Hope you will enjoy it.',
     contacts: {
-      email: 'anextep@gmail.com',
+      email: 'youranextep@gmail.com',
       facebook: '#',
       telegram: '#',
       twitter: '#',
--- content/pages/contacts.md
+++ content/pages/contacts.md
@@ -7,7 +7,7 @@ socialImage: "/media/image-4.png"
 <div style="text-align:center">
 <span>Any questions? Have a Suggestion? Anything</span>
 I am always here for you and I really appreciate the feedback.
-Email me at <a href="anextep@gmail.com">anextep@gmail.com</a>
+Email me at <a href="youranextep@gmail.com">youranextep@gmail.com</a>
 </div>
 
 ![contact Anextep Image](/media/image-4.png)
